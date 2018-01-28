9745fdea9bf8d2cfd0b2b5be31f238770f45e364
Benedikt Passini <pasbeb10@gmail.com>
2018-01-28 22:34:35 +0100

    Main activity Screen

--- app/src/main/java/at/passini/ballnavigator/MainActivity.java
+++ app/src/main/java/at/passini/ballnavigator/MainActivity.java
@@ -3,15 +3,27 @@ package at.passini.ballnavigator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(MainActivity.this, GameActivity.class);
-        startActivity(intent);
+
+        btnStart = (Button) findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
