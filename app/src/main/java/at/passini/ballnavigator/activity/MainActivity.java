d643df6440a96ac86f4626967a4c05aa6a6ff381
Benedikt Passini <pasbeb10@gmail.com>
2018-01-30 18:43:04 +0100

    add btns

--- app/src/main/java/at/passini/ballnavigator/activity/MainActivity.java
+++ app/src/main/java/at/passini/ballnavigator/activity/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import at.passini.ballnavigator.R;
 
@@ -12,14 +13,28 @@ public class MainActivity extends AppCompatActivity {
 
     private Button btnStart;
     private Button btnAchievements;
+    private Button btnShop;
+    private ImageButton btnHelp;
+    private ImageButton btnSetting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initButtons();
+        initButtonListener();
 
+    }
 
+    private void initButtons(){
         btnStart = (Button) findViewById(R.id.btnStart);
+        btnAchievements = (Button) findViewById(R.id.btnAchievements);
+        btnShop = (Button) findViewById(R.id.btnOpenShop);
+        btnHelp = (ImageButton) findViewById(R.id.btnOpenHelp);
+        btnSetting = (ImageButton) findViewById(R.id.btnOpenSetting);
+    }
+
+    private void initButtonListener(){
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -28,7 +43,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        btnAchievements = (Button) findViewById(R.id.btnAchievements);
         btnAchievements.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -37,5 +51,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btnShop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnHelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
